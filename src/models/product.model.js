bf62d440ee814d4a5c38df9c308b1a090f0304e4
Gulashan Hashami <gulashan959898@gmail.com>
2022-05-13 05:28:01 +0530

    added

---
+++ src/models/product.model.js
@@ -0,0 +1,18 @@
+const mongoose=require("mongoose");
+const productSchema=new mongoose.Schema(
+    {
+        name: { type: String, require: true },
+        symbol: { type: String, require: true },
+        current_price: { type: Number, require: true },
+        market_cap_rank: { type: Number, require: true },
+        total_volume: { type: Number, require: true },
+        total_supply: { type: Number, require: true },
+        image: { type: String, require: true },
+       
+    },
+    {
+        versionKey:false,
+        timestamps:true,
+    }
+);
+module.exports= mongoose.model("product", productSchema);
