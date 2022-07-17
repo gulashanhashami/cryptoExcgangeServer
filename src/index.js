a0c6d1297f1ff9b746ba6f50369b7689bdc500fb
Gulashan Hashami <gulashan959898@gmail.com>
2022-07-17 12:28:30 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const express=require("express");
+const app=express();
+const cors = require('cors'); 2
 const productController=require("./controllers/product.controller")
 
-const app=express();
+
 app.use(express.json())
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+  }
+  app.use(cors(corsOptions));
 app.use("/products", productController);
 
 module.exports=app;
